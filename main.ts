9222d1a5b0f17d658f6a83f2e94351fa4a349f43
HuangXiao12333 <67530476+HuangXiao12333@users.noreply.github.com>
2023-08-15 15:59:34 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
-})
+//% weight=100  color=#00A654   block="Light_up" icon="\uf067"
+namespace ledTricks {
+    /**
+    * Blinks the LED once.
+    */
+    //% block
+    export function blink() {
+        led.point(1, 1)
+        pause(500)
+        led.point(5, 5)
+        pause(500)
+    }
+}
